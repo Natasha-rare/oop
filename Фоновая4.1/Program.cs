f4cbb29d2a81546aa22fa29a5f6ca30e2477d2df
Natasha-rare <natasha_ea5@mail.ru>
2020-02-12 09:53:40 +0300

    Фоновая 4.1 Улучшена проверка пересечения + добавлен конструктор прямоугольника

--- Фоновая4.1/Program.cs
+++ Фоновая4.1/Program.cs
@@ -30,6 +30,15 @@ namespace Фоновая_4
             this.x = x;
             this.y = y;
         }
+
+        public Parallelepiped(int sh, int g, int x, int y)
+        {
+            this.sh = sh;
+            this.g = g;
+            this.h = 0;
+            this.x = x;
+            this.y = y;
+        }
         //Вывод длин сторон
         public void Print()
         {
@@ -91,8 +100,8 @@ namespace Фоновая_4
             int top = Math.Min(this.y + this.sh, obj.y + obj.sh);
             int right = Math.Min(this.x + this.g, obj.x + obj.g);
             int bottom = Math.Max(this.y, obj.y);
-            int width = right - left;
-            int g = top - bottom;
+            int g = right - left;
+            int width = top - bottom;
             return new Parallelepiped(width, g, this.h, left, bottom);
         }
     }
@@ -106,69 +115,90 @@ namespace Фоновая_4
         static Parallelepiped Create_Figure()
         {
             Console.WriteLine("Вы хотите самостоятельно ввести длины сторон параллепипеда (1)\nили создать фигуру по умолчанию (2)?");
-            byte answer;
+            int answer;
             // Просим выбрать конструктор объекта
             do
             {
                 Console.WriteLine("Введите выбранный вариант (1 или 2)");
-                answer = byte.Parse(Console.ReadLine());
+                answer = int.Parse(Console.ReadLine());
+                if (answer == 543210) Environment.Exit(0);
             }
             while (answer != 1 && answer != 2);
 
             if (answer == 1)
             {
                 // Вводим координаты точки
-                Console.WriteLine("Введите длины сторон параллелипипеда");
+                // Console.WriteLine("Введите длины сторон параллелипипеда");
+
+                Console.WriteLine("Введите длины сторон прямоугольника");
 
                 Console.Write("ширина=");
                 int sh = int.Parse(Console.ReadLine());
 
+                //Завершение работы
+                if (sh == 543210) Environment.Exit(0);
+
                 Console.Write("длина=");
                 int g = int.Parse(Console.ReadLine());
 
-                Console.Write("высота=");
-                int h = int.Parse(Console.ReadLine());
+                //Console.Write("высота=");
+                //int h = int.Parse(Console.ReadLine());
 
                 Console.Write("Точка вставки(x)=");
                 int x = int.Parse(Console.ReadLine());
 
                 Console.Write("Точка вставки(y)=");
                 int y = int.Parse(Console.ReadLine());
-                return new Parallelepiped(sh, g, h, x, y);
+                
+                return new Parallelepiped(sh, g, x, y);
             }
             return new Parallelepiped();
+        }
+
+        static void Peresek(Parallelepiped figure, Parallelepiped figure_2)
+        {
+            if (Cross(figure.Get_X(), figure.Get_X() + figure.Get_Sh(), figure_2.Get_X(), figure_2.Get_X() + figure_2.Get_Sh()))
+            {
+                Parallelepiped new_f = figure.Equal(figure_2);
+                new_f.Print();
+                if (new_f.Get_G() <= 0 || new_f.Get_Sh() <= 0) Console.WriteLine("Фигуры не пересекаются");
+                else Console.WriteLine("Пересечиние: g={0} sh={1}", new_f.Get_G(), new_f.Get_Sh());
+            }
+            else
+                Console.WriteLine("Фигуры не пересекаются");
         }
         static void Main(string[] args)
         {
             Parallelepiped figure = Create_Figure();
             figure.Print();
-            Console.WriteLine("Объем = {0}", figure.Volume());
-            Console.WriteLine("Площадь поверхности = {0}", figure.Area());
-            Console.WriteLine("Ширина={0}", figure.Get_Sh());
-            Console.WriteLine("Высота={0}", figure.Get_H());
-            Console.WriteLine("Длина(глубина)={0}", figure.Get_G());
+            //Console.WriteLine("Объем = {0}", figure.Volume());
+            //Console.WriteLine("Площадь поверхности = {0}", figure.Area());
+            //Console.WriteLine("Ширина={0}", figure.Get_Sh());
+            //Console.WriteLine("Высота={0}", figure.Get_H());
+            //Console.WriteLine("Длина(глубина)={0}", figure.Get_G());
 
-            string answer;
-            do
+            //string answer;
+            //do
+            //{
+            //    Console.WriteLine("Введите сторону, которую вы хотите изменить (ширина или глубина)");
+            //    answer = Console.ReadLine();
+            //}
+            //while (answer != "ширина" && answer != "глубина");
+
+            //int value;
+            //Console.WriteLine("Введите величину, на которую вы хотите изменить прямоугольник");
+            //value = int.Parse(Console.ReadLine());
+            ////Завершение работы
+            //if (value == 543210) Environment.Exit(0);
+            //figure.Resize(answer, value);
+            while (true)
             {
-                Console.WriteLine("Введите сторону, которую вы хотите изменить (ширина или глубина)");
-                answer = Console.ReadLine();
+                Parallelepiped figure_2 = Create_Figure();
+                Peresek(figure, figure_2);
             }
-            while (answer != "ширина" && answer != "глубина");
-
-            int value;
-            Console.WriteLine("Введите величину, на которую вы хотите изменить прямоугольник");
-            value = int.Parse(Console.ReadLine());
-            figure.Resize(answer, value);
-            Parallelepiped figure_2 = Create_Figure();
+            
             //figure_2.Print();
-            if (Cross(figure.Get_X(), figure.Get_X() + figure.Get_Sh(), figure_2.Get_X(), figure_2.Get_X() + figure_2.Get_Sh()))
-            {
-                Parallelepiped new_f = figure.Equal(figure_2);
-                new_f.Print();
-            }
-            else
-                Console.WriteLine("Фигуры не пересекаются");
+            
         }
     }
 }
