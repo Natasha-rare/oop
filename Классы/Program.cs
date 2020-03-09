79abfd67728f0359320f231a057af3121ae6fd19
Natasha-rare <natasha_ea5@mail.ru>
2020-03-09 10:50:40 +0300

    Обработка исключений (часть 4)

--- Классы/Program.cs
+++ Классы/Program.cs
@@ -4,20 +4,21 @@ namespace Классы
 {
     class Point3D
     {
-        private double x;
-        private double y;
-        private double z;
+        private int x;
+        private int y;
+        private int z;
 
         public Point3D()
         {
-            x = 0; y = 0; z = 0;
+
         }
 
-        public Point3D(double x1, double y1, double z1)
+        public Point3D(int x, int y, int z)
         {
-            x = x1;
-            y = y1;
-            z = z1;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
         }
 
         public void Print_Point()
@@ -25,70 +26,243 @@ namespace Классы
             Console.WriteLine("Координаты точки: x={0} y={1} z={2}", x, y, z);
         }
 
-        public void Move_Point(string move, double len)
+        public void Move_Point(int move, int len)
         {
             switch (move)
             {
-                case "x":
+                case 1:
                     x += len;
                     break;
-                case "y": y += len;
+                case 2:
+                    y += len;
                     break;
-                case "z": z += len;
+                case 3:
+                    z += len;
                     break;
             }
         }
+
+        public double Len_Vec
+        {
+            get
+            {
+                return Math.Sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+            }
+        }
+
+        public void Sum(Point3D obj)
+        {
+            this.x += obj.x;
+            this.y += obj.y;
+            this.z += obj.z;
+        }
+
+        public void Sum(int n)
+        {
+            x += n;
+            y += n;
+            z += n;
+        }
+
+        public void Sum(int x, int y, int z)
+        {
+            this.x += x;
+            this.y += y;
+            this.z += z;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                try
+                {
+                    if (value >= 0) x = value;
+                    else throw new Exception("Присваимое значение должно быть не отрицательным");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Ошибка: {0}", error.Message);
+                }
+
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                try
+                {
+                    if (value >= 0 && value <= 100) y = value;
+                    else throw new Exception("Значение не входит в диапазон от нуля до 100. Координате присвоено значение 100");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Ошибка: {0}", error.Message);
+                    y = 100;
+                }
+
+            }
+        }
+
+        public int Z
+        {
+            get
+            {
+                return z;
+            }
+
+            set
+            {
+                try
+                {
+                    if (value <= x + y) z = value;
+                    else throw new Exception("Превышено возможное значение");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Ошибка: {0}", error.Message);
+                }
+            }
+        }
+
+        public int Increase
+        {
+            set
+            {
+                z *= value;
+                x *= value;
+                y *= value;
+            }
+        }
+
 
     }
     class Program
     {
         static Point3D Create_Point()
         {
-            Point3D new_point = new Point3D();
             Console.WriteLine("Вы хотите самостоятельно ввести координаты объекта (1)\nили создать объект по умолчанию (2)?");
             byte answer;
-            // Просим выбрать конструктор объекта
-            do
+            // Просим выбрать конструктор объекта
+            do
             {
                 Console.WriteLine("Введите выбранный вариант (1 или 2)");
                 answer = byte.Parse(Console.ReadLine());
             }
             while (answer != 1 && answer != 2);
-
-            switch (answer)
+            int x, y, z;
+            string s;
+            if (answer == 1)
             {
-                case 1:
-                    // Вводим координаты точки
-                    Console.WriteLine("Введите координату точки");
+                // Вводим координаты точки
+                Console.WriteLine("Введите координату точки");
 
+                do
+                {
                     Console.Write("x=");
-                    double x = double.Parse(Console.ReadLine());
+                    s = Console.ReadLine();
+                } while (!int.TryParse(s, out x));
 
+                do
+                {
                     Console.Write("y=");
-                    double y = double.Parse(Console.ReadLine());
+                    s = Console.ReadLine();
+                } while (!int.TryParse(s, out y));
 
+                do
+                {
                     Console.Write("z=");
-                    double z = double.Parse(Console.ReadLine());
-                    new_point = new Point3D(x, y, z);
-                    break;
-                case 2:
-                    new_point = new Point3D();
-                    break;
+                    s = Console.ReadLine();
+                } while (!int.TryParse(s, out z));
+
+                try
+                {
+                    if (x % 5 != 0 && y % 5 != 0 && z % 5 != 0) throw new Exception("Ни одна из координат не кратна 5. Создается объект с координатами (5, 5, 5)");
+                    else return new Point3D(x, y, z);
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Ошибка: {0}", error.Message);
+                    return new Point3D(5, 5, 5);
+                }
+
             }
-            return new_point;
+            return new Point3D();
         }
         static void Main(string[] args)
         {
-            Point3D new_point = Create_Point(); 
-            // Вывод точки
-            new_point.Print_Point();
-            // Двигаем точку
-            Console.Write("Введите ось координат (x, y или z) ");
-            string move = Console.ReadLine();
-            Console.Write("Введите расстояние ");
-            double len = double.Parse(Console.ReadLine());
+            Point3D new_point = Create_Point();
+            // Вывод точки
+            new_point.Print_Point();
+            int move, len;
+            int x, y, z;
+            string s;
+            new_point.Y = 1022;
+            // Двигаем точку
+            do
+            {
+                Console.Write("Введите ось координат (x(1), y(2) или z(3)) ");
+                move = int.Parse(Console.ReadLine());
+            }
+            while (move != 1 && move != 2 && move != 3);
+
+            do
+            {
+                Console.Write("Введите расстояние ");
+                s = Console.ReadLine();
+            } while (!int.TryParse(s, out len));
             new_point.Move_Point(move, len);
 
+            new_point.Print_Point();
+
+            double r = new_point.Len_Vec;
+            Console.WriteLine("Длина радиус-вектора {0}", r);
+
+            Console.WriteLine("\nСоздаем новую точку\n");
+            Point3D new_point_2 = Create_Point();
+            new_point.Sum(new_point_2);
+            Console.WriteLine("После сложения точек координаты большей равны:");
+            new_point.Print_Point();
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Введите число, на которое нужно увеличить все координаты точки ");
+                s = Console.ReadLine();
+            } while (!int.TryParse(s, out move));
+            new_point.Sum(move);
+            new_point.Print_Point();
+            Console.WriteLine("Введите числа, на которые нужно изменить координату");
+            do
+            {
+                Console.Write("x=");
+                s = Console.ReadLine();
+            } while (!int.TryParse(s, out x));
+
+            do
+            {
+                Console.Write("y=");
+                s = Console.ReadLine();
+            } while (!int.TryParse(s, out y));
+
+            do
+            {
+                Console.Write("z=");
+                s = Console.ReadLine();
+            } while (!int.TryParse(s, out z));
+            new_point.Sum(x, y, z);
             new_point.Print_Point();
         }
     }
