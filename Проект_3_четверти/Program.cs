1188b1503d6cee8c5ca5093e3b72d131c76102a5
Natasha-rare <natasha_ea5@mail.ru>
2020-04-03 16:15:08 +0300

    Класс Карты процесс разработки. Неправильное запол нение массива.StackOverFlow exception!

--- Проект_3_четверти/Program.cs
+++ Проект_3_четверти/Program.cs
@@ -2,29 +2,67 @@
 
 namespace Проект_3_четверти
 {
-    enum cards
+    public enum Pack
     {
-        Шестерка=1, Семерка,
+        Шестерка=6, Семерка,
         Восьмерка, Девятка, Десятка, Валет, Дама, Король, Туз
     }
 
+
     class Cards
     {
-        private int cards;
-        private int[] count;
+        Pack[] cards = new Pack[36];
+        static Random rnd = new Random();
+
+        public Cards()
+        {
+            
+            for (int i = 0; i<cards.Length; i++)
+            {
+                cards[i] = Generate_Cards();
+            }
+        }
+
+        public Pack Generate_Cards()
+        {
+            Pack n;
+            n = (Pack)rnd.Next(6, 15);
+            if (Repeat(n, this.cards))  return n;
+            return Generate_Cards();
+        }
+        public void Print()
+        {
+            
+            foreach(Pack x in cards)
+            {
+                Console.WriteLine(Convert.ToString(x));
+            }
+        }
+
+        public bool Repeat(Pack n, Pack [] cards)
+        {
+            int count = 0;
+            foreach(Pack i in cards)
+            {
+                if (n == i) count += 1;
+            }
+            return count < 3;
+        }
     }
 
-    class Player
-    {
-        private int[] cards;
-        private int number_of_cards;
-    }
+    //class Player
+    //{
+    //    private int[] cards;
+    //    private int number_of_cards;
+
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-
+            Cards c = new Cards();
+            c.Print();
         }
     }
 }
