fd6fe72ac8e4f792a6af88da016a686d99e58748
Natasha-rare <natasha_ea5@mail.ru>
2020-04-15 10:33:52 +0300

    * Классы.sln: * Transport.csproj: * Program.cs:
    * Program.cs: Начало работы над фоновой 5. 1 -- Базовый класс
      Транспортное средство

---
+++ Transport/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Transport
+{
+    class Transport
+    {
+        protected string name; // название
+        protected int spend_fuel; // трата топлива
+        protected int container_fuel; // емкость топливного бака
+
+        // конструктор по умолчанию
+        public Transport()
+        {
+            spend_fuel = 10;
+            container_fuel = 50;
+        }
+
+        // конструктор с параметрами
+        public Transport(string name, int spend_fuel, int container_fuel)
+        {
+            this.name = name;
+            this.spend_fuel = spend_fuel;
+            this.container_fuel = container_fuel;
+        }
+
+        public string Name //  название
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Max_Len // дальность поездки
+        {
+            get
+            {
+                return container_fuel / spend_fuel;
+            }
+        }
+
+        public int Need_Fuel(int length)
+        {
+            return (length * spend_fuel) / 100;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
